fee5917c07ee8ee044eec7a0a4146d5aeeebe70a
DitHB <dasmutterschiff@hotmail.com>
2013-09-30 14:22:28 +0200

    Brugere fix

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -5,10 +5,53 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+using System.Web.Security;
+
+
 public partial class _Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
 
     }
+
+
+    protected void LinkButton_Login_Click(object sender, EventArgs e)
+    {
+        // opret forbindelsen til databasen
+        SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ToString());
+
+        // opret et SqlCommand object
+        SqlCommand cmd = new SqlCommand();
+        cmd.Connection = conn;
+
+        //SQL-Command
+        cmd.CommandText = "SELECT * from brugere WHERE Email=@login AND Password=@pass";
+        cmd.Parameters.Add("@login", SqlDbType.NVarChar).Value = TextBox_Email.Text;
+        cmd.Parameters.Add("@pass", SqlDbType.NVarChar).Value = TextBox_Password.Text;
+
+        // åben forbindelsen til databasen
+        conn.Open();
+
+        // opret en SqlDataReader og navngiv den "reader"
+        //sqldatareader er et object som kommunikerer med databasen
+        // SqlDataReader er en beholder eller tabel der indeholder en kopi af det der er udtrukket fra database
+        SqlDataReader reader = cmd.ExecuteReader();
+        if (reader.Read())
+        {
+            Session["Bruger_id"] = reader["Id"];
+            Session["Navn"] = reader["Navn"];
+            Response.Redirect("Brugere.aspx");
+        }
+        else // hvis brugeren ikke findes i databasen
+        {
+                Label_Besked.Text = "<div class='span8'><div class='alert alert-error'>Kombinationen af din email og password findes ikke i databasen</div></div>";
+        }
+        // luk forbindelsen til databasen
+        conn.Close();
+    }
+
 }
